b4511b5ae2ebd1548446b585bf5c725b10f0f9bd
aaronrobertson <aaronvincentrobertson@gmail.com>
2015-12-20 23:25:36 +0000

    initial application commit with manual input

--- 2/main.java
+++ 2/main.java
@@ -1,1 +1,35 @@
-
+import java.util.Scanner;
+import java.util.Arrays;
+class main{
+    
+    public static void main(String[] args){
+       
+        int[] dimensions = {0,0,0};
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Enter package length: ");
+        dimensions[0] = in.nextInt();
+        System.out.println("Enter package width: ");
+        dimensions[1] = in.nextInt();
+        System.out.println("Enter package height: ");
+        dimensions[2] = in.nextInt();
+        
+        
+        System.out.println("Total required wrapping paper (sqft): "+surfaceArea(dimensions));
+        
+    }
+    
+    public static int surfaceArea(int[] d){
+        
+        int l = d[0], w = d[1], h = d[2];
+        int SA = 2*((l*w)+(w*h)+(h*l));
+        
+        Arrays.sort(d);
+        
+        int EXTRA = d[0]*d[1];
+        
+        return SA+EXTRA;
+        
+    }
+    
+}
